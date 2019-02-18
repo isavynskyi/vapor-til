436a83528ea8df56cda2a35d2c440210b6490e99
IhorS <wadedunk08@gmail.com>
2019-02-18 19:11:29 +0200

    Use PostgreSQL as the database

--- Sources/App/Models/Acronym.swift
+++ Sources/App/Models/Acronym.swift
@@ -1,6 +1,5 @@
 import Vapor
-import FluentSQLite
-
+import FluentPostgreSQL
 final class Acronym: Codable {
     var id: Int?
     var short: String
@@ -10,7 +9,6 @@ final class Acronym: Codable {
         self.long = long
     }
 }
-
-extension Acronym: SQLiteModel {}
+extension Acronym: PostgreSQLModel {}
 extension Acronym: Migration {}
 extension Acronym: Content {}
